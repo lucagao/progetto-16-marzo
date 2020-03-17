da2969228a89b478673f9e0d9b38c654585d2e30
lucagao <lucagao11@gmail.com>
2020-03-17 14:50:40 +0100

    nuo progetto

---
+++ progettologin-16-marzo-1/src/main/java/com/gpch/login/Progettologin16Marzo1Application.java
@@ -0,0 +1,13 @@
+package com.gpch.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettologin16Marzo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettologin16Marzo1Application.class, args);
+	}
+
+}
---
+++ progettologin-16-marzo-1/src/test/java/com/gpch/login/Progettologin16Marzo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpch.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettologin16Marzo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
